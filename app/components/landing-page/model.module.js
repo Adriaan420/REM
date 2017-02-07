98be4f89f7ac1eb385ee321bf66f49c75812d3a6
Adriaan420 <dannypijlman@gmail.com>
2017-02-07 10:54:42 +0100

    a
    BBBBBBBBBBBBBB

--- app/components/landing-page/model.module.js
+++ app/components/landing-page/model.module.js
@@ -6,26 +6,6 @@
 var app = angular.module('landingPage', ['ngMaterial','ngMessages','ui.bootstrap','ngSanitize'])
 //  define the 'landingPage' module
 
-        app.controller('AppCtrl', function($scope, $sce) {
-        $scope.status = '  ';
-        $scope.customFullscreen = false;
-        $scope.htmlPopover = $sce.trustAsHtml();
-
-        $scope.showAlert = function (ev) {
-            // Appending dialog to document.body to cover sidenav in docs app
-            // Modal dialogs should fully cover application
-            // to prevent interaction outside of dialog
-            $mdDialog.show(
-                $mdDialog.alert()
-                    .parent(angular.element(document.querySelector('#popupContainer')))
-                    .clickOutsideToClose(true)
-                    .title('Coal Power')
-                    .textContent('Dit project geeft mij een ernstige hersentumor')
-                    .ariaLabel('Alert Dialog Demo')
-                    .targetEvent(ev)
-            );
-        };
-    });
         app.controller('SideMenu', function ($scope, $timeout, $mdSidenav) {
         $scope.toggleLeft = buildToggler('left');
         $scope.toggleRight = buildToggler('right');
@@ -36,4 +16,3 @@ var app = angular.module('landingPage', ['ngMaterial','ngMessages','ui.bootstrap
             }
         }
     });
-
